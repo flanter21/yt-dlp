1cbaceb134d1b306de0fcd7f1cc29a34a1ec86d3
flanter21 <flanter21@gmail.com>
2025-02-13 18:43:21 +0000

    [ie/BlackboardCollaborate] Add support for subtitles, live chat, filesize and videos behind a login wall
    Co-authored-by: bashonly <88596187+bashonly@users.noreply.github.com>

--- yt_dlp/extractor/blackboardcollaborate.py
+++ yt_dlp/extractor/blackboardcollaborate.py
@@ -1,16 +1,28 @@
 from .common import InfoExtractor
-from ..utils import parse_iso8601
+from ..utils import (
+    UnsupportedError,
+    int_or_none,
+    float_or_none,
+    join_nonempty,
+    jwt_decode_hs256,
+    mimetype2ext,
+    parse_iso8601,
+    parse_qs,
+    url_or_none,
+)
+from ..utils.traversal import traverse_obj
+
 
 
 class BlackboardCollaborateIE(InfoExtractor):
     _VALID_URL = r'''(?x)
                         https?://
-                        (?P<region>[a-z-]+)\.bbcollab\.com/
+                        (?P<region>[a-z]+)(?:-lti)?\.bbcollab\.com/
                         (?:
                             collab/ui/session/playback/load|
                             recording
                         )/
-                        (?P<id>[^/]+)'''
+                        (?P<id>[^/?#]+)'''
     _TESTS = [
         {
             'url': 'https://us-lti.bbcollab.com/collab/ui/session/playback/load/0a633b6a88824deb8c918f470b22b256',
@@ -42,22 +54,53 @@ class BlackboardCollaborateIE(InfoExtractor):
         },
     ]
 
+    def _call_api(self, region, video_id, path=None, token=None, note=None, fatal=False):
+        # Ref: https://github.com/blackboard/BBDN-Collab-Postman-REST
+        return self._download_json(
+            join_nonempty(f'https://{region}.bbcollab.com/collab/api/csa/recordings', video_id, path, delim='/'),
+            video_id, note or 'Downloading JSON metadata', fatal=fatal,
+            headers={'Authorization': f'Bearer {token}'} if token else None)
+
     def _real_extract(self, url):
         mobj = self._match_valid_url(url)
         region = mobj.group('region')
         video_id = mobj.group('id')
-        info = self._download_json(
-            f'https://{region}.bbcollab.com/collab/api/csa/recordings/{video_id}/data', video_id)
-        duration = info.get('duration')
-        title = info['name']
-        upload_date = info.get('created')
-        streams = info['streams']
-        formats = [{'format_id': k, 'url': url} for k, url in streams.items()]
+        token = parse_qs(url).get('authToken', [None])[-1]
+
+        video_info = self._call_api(region, video_id, path='data/secure', token=token, note='Trying auth token')
+        if video_info:
+            video_extra = self._call_api(region, video_id, token=token, note='Retrieving extra attributes')
+        else:
+            video_info = self._call_api(region, video_id, path='data', note='Trying fallback', fatal=True)
+            video_extra = {}
+
+        formats = traverse_obj(video_info, ('extStreams', lambda _, v: url_or_none(v['streamUrl']), {
+            'url': 'streamUrl',
+            'ext': ('contentType', {mimetype2ext}),
+            'aspect_ratio': ('aspectRatio', {float_or_none}),
+        }))
+
+        if filesize := traverse_obj(video_extra, ('storageSize', {int_or_none})):
+            for fmt in formats:
+                fmt['filesize'] = filesize
+
+        subtitles = {}
+        for subs in traverse_obj(video_info, ('subtitles', lambda _, v: url_or_none(v['url']))):
+            subtitles.setdefault(subs.get('lang') or 'und', []).append({
+                'name': traverse_obj(subs, ('label', {str})),
+                'url': subs['url'],
+            })
+
+        for live_chat_url in traverse_obj(video_info, ('chats', ..., 'url', {url_or_none})):
+            subtitles.setdefault('live_chat', []).append({'url': live_chat_url})
 
         return {
-            'duration': duration,
+            **traverse_obj(video_info, {
+                'title': ('name', {str}),
+                'timestamp': ('created', {parse_iso8601}),
+                'duration': ('duration', {int_or_none(scale=1000)}),
+            }),
             'formats': formats,
             'id': video_id,
-            'timestamp': parse_iso8601(upload_date),
-            'title': title,
+            'subtitles': subtitles,
         }
